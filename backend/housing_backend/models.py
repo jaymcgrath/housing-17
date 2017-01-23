f659199eeb1c18165030b6f61dc8b5dfd82ac08a
Riley Rustad <rileyrustad@gmail.com>
2017-01-23 11:38:27 -0800

    added models for the mvp

--- backend/housing_backend/models.py
+++ backend/housing_backend/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Affordable(models.Model):
+    affordable = models.BooleanField()
+    demographic = models.ForeignKey('Demographic', on_delete=models.CASCADE)
+    housing_size = models.ForeignKey('HousingSize', on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey('Neighborhood', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.group_typ + self.group_type + self.housing_size
+
+
+class Demographic(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class HousingSize(models.Model):
+    household_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.household_type
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    report_year = models.ForeignKey('ReportYear', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ReportYear(models.Model):
+    year = models.IntegerField()
+
+    def __str__(self):
+        return str(self.year)
