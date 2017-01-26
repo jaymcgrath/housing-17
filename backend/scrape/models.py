b9ee779ae84566a68975506b9d834403f5ee5fa8
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-25 18:20:29 -0800

    change price to rent, changed to IntegerField

--- backend/scrape/models.py
+++ backend/scrape/models.py
@@ -2,19 +2,19 @@ from decimal import Decimal
 
 from django.db import models
 
-
-class CurrencyField(models.DecimalField):
-    """
-    Custom currency field, returns two decimal places
-    """
-
-    __metaclass__ = models.SubfieldBase
-
-    def to_python(self, value):
-        try:
-            return super(CurrencyField, self).to_python(value).quantize(Decimal("0.01"))
-        except AttributeError:
-            return None
+#TODO: fix this for django 1.10, then assign to price (currently IntegerField)
+# class CurrencyField(models.DecimalField):
+#     """
+#     Custom currency field, returns two decimal places
+#     """
+#
+#     __metaclass__ = models.SubfieldBase
+#
+#     def to_python(self, value):
+#         try:
+#             return super(CurrencyField, self).to_python(value).quantize(Decimal("0.01"))
+#         except AttributeError:
+#             return None
 
 
 class CraigslistPosting(models.Model):
@@ -25,7 +25,7 @@ class CraigslistPosting(models.Model):
     created_on = models.DateTimeField(auto_now_add=True, help_text='when the record was created')
     listed_on = models.DateTimeField(help_text='when the craigslist listing was posted')
     district = models.CharField(max_length=256, help_text='neighborhood according to craigslist')
-    price = CurrencyField(help_text='monthly rent')
+    price = models.IntegerField(help_text='monthly rent')
     bedrooms = DecimalField(null=True, help_text='number of bedrooms')
     bathrooms = DecimalField(null=True, help_text='number of bathrooms') # Decimal to handle 1.5 bath, etc
     sq_ft = IntegerField(null=True, help_text='square footage')
