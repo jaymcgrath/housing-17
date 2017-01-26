6b62c1ce3da2ee0e9486b8fa8044a811cb84eba0
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-01-25 16:48:48 -0800

    add created_on, listed_on, and sq_ft fields to CraigslistPosting model

--- backend/scrape/models.py
+++ backend/scrape/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from decimal import Decimal
 
 class CurrencyField(models.DecimalField):
+    "Custom currency field, returns two decimal places"
     __metaclass__ = models.SubfieldBase
 
     def to_python(self, value):
@@ -17,12 +18,18 @@ class CurrencyField(models.DecimalField):
 class CraigslistPosting(models.Model):
     """
     Model for an individual housing listing scraped from Craigslist
+
     """
+    created_on = models.DateTimeField(auto_now_add=True, help_text='when the record was created')
+    listed_on = models.DateTimeField(help_text='when the craigslist listing was posted')
     district = models.CharField(max_length=256, help_text='neighborhood according to craigslist')
     price = CurrencyField(help_text='monthly rent')
-    bedrooms = DecimalField(null=True, help_text='Number of bedrooms')
-    bathrooms = DecimalField(null=True, help_text='Number of bathrooms')
-    url = TextField(null=True, help_text='URL from which this info was scraped')
-    title = TextField(null=True, help_text='original title of the listing')
-    lon = models.FloatField(help_text='longitude')
-    lat = models.FloatField(help_text='latitude')
+    bedrooms = DecimalField(null=True, help_text='number of bedrooms')
+    bathrooms = DecimalField(null=True, help_text='number of bathrooms') # Decimal to handle 1.5 bath, etc
+    sq_ft = IntegerField(null=True, help_text='square footage')
+    url = TextField(help_text='url of original listing from which this info was scraped')
+    title = TextField(help_text='original title of the listing')
+    lat = models.FloatField(null=True, help_text='latitude')
+    lon = models.FloatField(null=True, help_text='longitude')
+
+
