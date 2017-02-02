0e36646d3d58e7aa6257ebbf0c0d3b959239b655
eric.almadova <eric.almadova@elementaltechnologies.com>
2017-02-01 23:22:42 -0800

    Broke out main functionality into a method

--- backend/housing_backend/loader.py
+++ backend/housing_backend/loader.py
@@ -8,31 +8,34 @@ https://drive.google.com/file/d/0B0810KzsNR3mUDkzdERQNmc3U00/view?usp=sharing
 #TODO:
 #   (1/31) - Update to take in new csv files: NeighborhoodProfiles, NeighborhoodHousingMarket and Demographic profiles
 
+def loadAffordability(file):
+    dframe = pd.read_csv(file)
+
+    ry,_ = ReportYear.objects.get_or_create(year=2016)
+    ry.save()
+
+    for neighborhood in dframe.Neighborhood.unique():
+        Neighborhood(name=neighborhood, report_year=ry)
+
+    for index, row in dframe.iterrows():
+        d, _ = Demographic.objects.get_or_create(name=row['Demographic'])
+        h, _ = HousingSize.objects.get_or_create(household_type=row['Unit_Size'])
+        n, _ = Neighborhood.objects.get_or_create(name=row['Neighborhood'],report_year=ry)
+        d.save()
+        h.save()
+        n.save()
+        if row['Affordable_ind'] == 'Y':
+            aff = True
+        elif row['Affordable_ind'] == 'N':
+            aff = False
+            a = Affordable(
+                affordable=aff,
+                demographic=d,
+                housing_size=h,
+                neighborhood=n,
+            )
+            a.save()
+
+### MAIN ###
 file = input('What is the absolute path to the downloaded csv file?: ')
-
-dframe = pd.read_csv(file)
-
-ry,_ = ReportYear.objects.get_or_create(year=2016)
-ry.save()
-
-for neighborhood in dframe.Neighborhood.unique():
-    Neighborhood(name=neighborhood, report_year=ry)
-
-for index, row in dframe.iterrows():
-    d, _ = Demographic.objects.get_or_create(name=row['Demographic'])
-    h, _ = HousingSize.objects.get_or_create(household_type=row['Unit_Size'])
-    n, _ = Neighborhood.objects.get_or_create(name=row['Neighborhood'],report_year=ry)
-    d.save()
-    h.save()
-    n.save()
-    if row['Affordable_ind'] == 'Y':
-        aff = True
-    elif row['Affordable_ind'] == 'N':
-        aff = False
-        a = Affordable(
-            affordable=aff,
-            demographic=d,
-            housing_size=h,
-            neighborhood=n,
-        )
-        a.save()
+loadAffordability(file)
