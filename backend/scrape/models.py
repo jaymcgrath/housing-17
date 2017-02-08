f70c3ecc6568acd75d617fb78dcae2e6dcafac39
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-02-07 19:49:17 -0800

    remove region from CL Posting model

--- backend/scrape/models.py
+++ backend/scrape/models.py
@@ -18,20 +18,15 @@ from django.db import models
 
 
 class CraigslistPosting(models.Model):
-    """
-    Model for an individual housing listing scraped from Craigslist
-
-    """
+    """ Model for an individual housing listing scraped from Craigslist """
     created_on = models.DateTimeField(auto_now_add=True, help_text='when the record was created')
     listed_on = models.DateTimeField(help_text='when the craigslist listing was posted')
-    district = models.CharField(max_length=256, help_text='neighborhood according to craigslist')
     rent = models.IntegerField(help_text='monthly rent')
-    bedrooms = models.DecimalField(null=True, help_text='number of bedrooms')
-    bathrooms = models.DecimalField(null=True, help_text='number of bathrooms') # Decimal to handle 1.5 bath, etc
+    # DecimalField to handle 1.5 bedroom or bathroom designations
+    bedrooms = models.DecimalField(null=True, max_digits=3, decimal_places=1, help_text='number of bedrooms')
+    bathrooms = models.DecimalField(null=True, max_digits=3, decimal_places=1, help_text='number of bathrooms')
     sq_ft = models.IntegerField(null=True, help_text='square footage')
     url = models.TextField(help_text='url of original listing from which this info was scraped')
     title = models.TextField(help_text='original title of the listing')
     lat = models.FloatField(null=True, help_text='latitude')
     lon = models.FloatField(null=True, help_text='longitude')
-
-
