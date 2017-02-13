bd4c842fcf8324d413815ac41da3a2842b42be31
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-02-13 14:26:57 -0800

    add name kwarg to urls for reverse lookups

--- backend/housing_backend/urls.py
+++ backend/housing_backend/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from housing_backend import views
 
 urlpatterns = [
-    url(r'^affordable/$', views.AffordableList.as_view()),
-    url(r'^affordable/(?P<pk>[0-9]+)/$', views.AffordableDetail.as_view()),
+    url(r'^affordable/$', views.AffordableList.as_view(), name='affordable_list'),
+    url(r'^affordable/(?P<pk>[0-9]+)/$', views.AffordableDetail.as_view(), name='affordable_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
