ffed04715bf548757e73156163743536b6ebb7e8
Riley Rustad <rileyrustad@gmail.com>
2017-02-16 17:56:05 -0800

    integrated uncustomized swagger view, which can be viewed at http://127.0.0.1:8000/housing_api/

--- backend/housing_backend/urls.py
+++ backend/housing_backend/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from housing_backend import views
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^affordable/$', views.AffordableList.as_view()),
     url(r'^affordable/(?P<pk>[0-9]+)/$', views.AffordableDetail.as_view()),
     url(r'^rent/$', views.RentList.as_view()),
+    url(r'^$', schema_view),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
