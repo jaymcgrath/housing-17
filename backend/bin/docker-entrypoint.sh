3f1742638c63b3e719fddec777028d2e769489ee
mikethecanuck <mikethecanuck@gmail.com>
2017-03-27 11:37:04 -0700

    re-enabling the commented commands in case there is some dependency hidden there that prevents the container from starting correctly

--- backend/bin/docker-entrypoint.sh
+++ backend/bin/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 echo  Running docker-entrypoint.sh...
 
-#./bin/getconfig.sh
-#python manage.py migrate --no-input
-#python manage.py collectstatic --no-input
+./bin/getconfig.sh
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
 gunicorn backend.wsgi:application -b :8000
