f729127efa46931e0e584eeadf353cf2543d1748
Jay McGrath <jaymcgrath@users.noreply.github.com>
2017-04-09 09:54:49 -0700

    convert NaNs to Nones before db insert

--- backend/housing_backend/loader.py
+++ backend/housing_backend/loader.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from .models import *
-from django.conf import settings
-import requests
-import io
-import os
+
 
 
 def loadAffordability(file):
-    dframe = pd.read_csv(file)
+    df = pd.read_csv(file)
+
+    # Convert NaN values to None for db insert
+    dframe = df.where((pd.notnull(df)), None)
+
+    # Convert NaN to none for db insertion
 
     for index, row in dframe.iterrows():
         ry, _ = ReportYear.objects.get_or_create(year=row['Year'])
-        n, _ = Neighborhood.objects.get_or_create(name=row['Neighborhood'],report_year=ry)
+        n, _ = Neighborhood.objects.get_or_create(name=row['Neighborhood'])
         d, _ = Demographic.objects.get_or_create(name=row['Demographic'])
         h, _ = HousingSize.objects.get_or_create(household_type=row['Unit_Size'])
         ry.save()
@@ -28,6 +30,7 @@ def loadAffordability(file):
             demographic=d,
             housing_size=h,
             neighborhood=n,
+            year=ry
         )
         a.save()
 
@@ -36,14 +39,16 @@ def loadDemographics(file):
     dframe = pd.read_csv(file)
 
     for index, row in dframe.iterrows():
-        demo, _ = Demographic.objects.get_or_create(
+            ry, _ = ReportYear.objects.get_or_create(year=row['DP_REPORTYEAR'])
+            demo, _ = Demographic.objects.get_or_create(
                 name=row['DP_TITLE'],
                 income_median=row['DP_INCOME_MEDIAN'],
                 housing_budget=row['DP_HOUSINGBUDGET'],
                 per_with_children=row['DP_PERCENT_WITH_CHILDREN'],
-                household_comp=row['DP_HOUSEHOLD_COMP']
-        )
-        demo.save()
+                household_comp=row['DP_HOUSEHOLD_COMP'],
+                year=ry,
+            )
+            demo.save()
 
 
 def loadNeighborhoodRent(file):
@@ -60,29 +65,28 @@ def loadNeighborhoodRent(file):
         rent, _ = NeighborhoodRent.objects.get_or_create(
                 nh_id=n,
                 housing_size=h,
-                rent_amt=row['NHM_Rent_Amt']
+                rent_amt=row['NHM_Rent_Amt'],
+                year = ry,
         )
         rent.save()
 
 
 
-# """ Neighborhoods are currently loaded in the loadAffordability function
-#
-#     When uncommenting this, also uncomment the function call at the bottom of the file
-#
-# """
-#
-#
-# # def loadNeighborhoodProfiles(file):
-# #     dframe = pd.read_csv(file)
-# #
-# #     for index, row in dframe.iterrows():
-# #         ry, _ = ReportYear.objects.get_or_create()
-# #         profile, _ = Neighborhood.objects.get_or_create(
-# #                 name=row['NP_Title'],
-# #                 report_year=ry
-# #         )
-# #         profile.save()
+""" Neighborhoods are currently loaded in the loadAffordability function
+
+    When uncommenting this, also uncomment the function call at the bottom of the file
+
+"""
+
+
+def loadNeighborhoodProfiles(file):
+    dframe = pd.read_csv(file)
+
+    for index, row in dframe.iterrows():
+        this_neighborhood, _ = Neighborhood.objects.get_or_create(
+                name=row['NP_Title']
+        )
+        this_neighborhood.save()
 
 
 
@@ -91,45 +95,43 @@ def loadNeighborhoodRent(file):
     from the Neighborhood model
 """
 
-# def loadHousingSupplyandPermits(file):
-#     """Load housing supply and permit data into respective models"""
-#
-#     dframe = pd.read_csv(file)
-#
-#     for index, row in dframe.iterrows():
-#         ry, _ = ReportYear.objects.get_or_create(year=row['ReportYear'])
-#         n, _ = Neighborhood.objects.get_or_create(name=row['Neighborhood'].strip())
-#         hs = HousingSupply(
-#                             neighborhood= n,
-#                             report_year= ry,
-#                             single_units= row['SingleFamilyUnits'],
-#                             multi_units= row['MultiFamilyUnits']
-#         )
-#         hs.save()
-#
-#         hp = HousingPermits(
-#                             neighborhood=n,
-#                             report_year=ry,
-#                             single_permits=row['SingleFamilyPermits'],
-#                             multi_permits=row['MultiFamilyPermits']
-#         )
-#         hp.save()
-#
+def loadHousingSupplyandPermits(file):
+    """Load housing supply and permit data into respective models"""
+
+    dframe = pd.read_csv(file)
+
+    for index, row in dframe.iterrows():
+        ry, _ = ReportYear.objects.get_or_create(year=row['ReportYear'])
+        n, _ = Neighborhood.objects.get_or_create(name=row['Neighborhood'].strip())
+        hs = HousingSupply(
+                            neighborhood=n,
+                            report_year=ry,
+                            single_units=row['SingleFamilyUnits'],
+                            multi_units=row['MultiFamilyUnits']
+        )
+        hs.save()
+
+        hp = HousingPermits(
+                            neighborhood=n,
+                            report_year=ry,
+                            single_permits=row['SingleFamilyPermits'],
+                            multi_permits=row['MultiFamilyPermits']
+        )
+        hp.save()
 
 
 
-### MAIN ###
-fileDemo = "https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/DemographicProfiles.csv"
+
+fileDemo = "https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/DemographicProfiles_w2015_Profiles_2-15-17.csv"
 fileNeighborhoods = "https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/NeighborhoodProfiles.csv"
 fileAfford = "https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/SoHAffordabilityDatabyNeighborhoodUpload.csv"
 fileRent = "https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/NeighborhoodHousingMarket.csv"
 fileSupply = "https://raw.githubusercontent.com/hackoregon/housing-backend/datasources/HousingSupplyAndPermits.csv"
 
-loadDemographics(fileDemo)
+loadDemographics(fileDemo) # Load first because loadAffordability depends on it
 
-#loadNeighborhoodProfiles(fileNeighborhoods)
+loadNeighborhoodProfiles(fileNeighborhoods)
 
 loadAffordability(fileAfford)
 loadNeighborhoodRent(fileRent)
-#loadHousingSupplyandPermits(fileSupply)
-
+loadHousingSupplyandPermits(fileSupply)
