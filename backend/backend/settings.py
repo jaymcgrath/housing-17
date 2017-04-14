8fbd828a495701347746bbc3cadbcee4fd5a2bbc
mikethecanuck <mikethecanuck@gmail.com>
2017-04-13 22:33:02 -0700

    tickling Travis with another commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,8 @@ from . import project_config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'public_secret_key'
+SECRET_KEY = project_config.DJANGO_SECRET_KEY
+#SECRET_KEY = 'public_secret_key'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
