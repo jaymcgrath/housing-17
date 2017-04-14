35fb6ec0e8b8df75785f50b6a4e2465fbc072025
mikethecanuck <mikethecanuck@gmail.com>
2017-04-13 22:28:17 -0700

    tickling Travis with another commit

--- backend/bin/docker-entrypoint.sh
+++ backend/bin/docker-entrypoint.sh
@@ -7,7 +7,6 @@ echo  Running docker-entrypoint.sh...
 
 # Disabled the migrate step to bring the container startup time down to acceptable levels
 #python manage.py migrate --no-input
-
 python manage.py collectstatic --no-input
 
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
